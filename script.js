132e214cd1f320d34dc6dedbc0a12e9e1269badf
Baba340 <115713984+Baba340@users.noreply.github.com>
2022-12-23 17:44:12 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 this.onload = function(){let v=video,b=body;b.style.backgroundColor="#000",v.width=this.innerWidth,
-v.style.top=(this.innerHeight-v.width*.5625)/2+"px",v.volume=0,v.autoplay=v.loop=!0;
+v.style.top=(this.innerHeight-v.width*.5625)/2+"px";
 setTimeout(()=>{b.style.backgroundColor="#fff",load.style.display="none",main.style.display="block";return;},3e3);}
