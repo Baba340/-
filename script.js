9f9aa622b07687e5a5d773591a275934fe9362d8
Baba340 <115713984+Baba340@users.noreply.github.com>
2023-01-17 23:27:17 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-this.onload = function(){let v=video,b=body;b.style.backgroundColor="#000",v.width=this.innerWidth,
-v.style.top=(this.innerHeight-v.width*.5625)/2+"px";
-setTimeout(()=>{b.style.backgroundColor="#fff",load.style.display="none",main.style.display="block";return;},3e3);}
+/*window.onload = function(){let v=video,b=body;b.style.backgroundColor="#000",v.width=this.innerWidth,
+v.style.top=(this.innerHeight-v.width*.5625)/2+"px";setTimeout(()=>{b.style.backgroundColor="#fff",
+load.style.display="none",main.style.display="block";return;},3000);}*/
+
+let video = document.getElementById("video");
+video.height = window.innerHeight;
+
+//window.width > video.width
+if(window.innerWidth > video.height*0.7013){
+    video.style.left = (window.innerWidth-video.height*0.7013)/2 +"px";
+}else alert("a");
