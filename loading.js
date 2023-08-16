7fb3a2bcee4d295d6a39e6857d79e8a4dcdbdac4
Baba340 <115713984+Baba340@users.noreply.github.com>
2023-08-16 23:09:13 +0900

    Update loading.js

--- loading.js
+++ loading.js
@@ -1,27 +1,26 @@
-window.onload = function () {
-  // //低電力モード検出
-  // document.getElementById("loading-video").play().catch((error) => {
-  //     Swal.fire({
-  //         html : "低電力モードではページが正常に動作しません",
-  //         icon : "warning",
-  //         showCancelButton : true,
-  //         confirmButtonText : "正常に動作させる",
-  //         cancelButtonText : "このまま続ける",
-  //         allowOutsideClick : false
-  //     }).then((result) => {
-  //         if(result.isConfirmed){
-  //             Swal.fire({
-  //                 html : "低電力モードを解除したら<br>「完了」を押してください", 
-  //                 confirmButtonText : "完了",
-  //                 allowOutsideClick : false
-  //             }).then((result2) => {
-  //                 if(result2.isConfirmed){
-  //                     window.location.reload ();
-  //                 }
-  //             })
-  //         }
-  //     });
-  // });
+window.addEventListener("DOMContentLoaded", () => {  // //低電力モード検出
+  document.getElementById("loading-video").play().catch((error) => {
+      Swal.fire({
+          html : "低電力モードではページが正常に動作しません",
+          icon : "warning",
+          showCancelButton : true,
+          confirmButtonText : "正常に動作させる",
+          cancelButtonText : "このまま続ける",
+          allowOutsideClick : false
+      }).then((result) => {
+          if(result.isConfirmed){
+              Swal.fire({
+                  html : "低電力モードを解除したら<br>「完了」を押してください", 
+                  confirmButtonText : "完了",
+                  allowOutsideClick : false
+              }).then((result2) => {
+                  if(result2.isConfirmed){
+                      window.location.reload ();
+                  }
+              })
+          }
+      });
+  });
   setTimeout(() => {
     //loadingを隠してcontentを見せる
     document.querySelector(".loading").classList.add("loaded");
@@ -32,4 +31,4 @@ window.onload = function () {
   setTimeout(() => {
     document.querySelector(".loading").remove();
   }, 3450);
-}
+});
