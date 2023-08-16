7607288d58acbcc64d7332741217a3f0ccf49571
Baba340 <115713984+Baba340@users.noreply.github.com>
2023-08-16 23:10:01 +0900

    Update script.js

--- script.js
+++ script.js
@@ -25,35 +25,35 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 //===.all-themeの表示/非表示(アコーディオンメニュー)
-function seeAllTheme(i){
-  const all_theme = document.querySelectorAll(".all-theme");
-  const button_arrow = document.querySelector(".button-arrow");
-  const see_all_theme = document.querySelectorAll(".see-all-theme");
+// function seeAllTheme(i){
+//   const all_theme = document.querySelectorAll(".all-theme");
+//   const button_arrow = document.querySelector(".button-arrow");
+//   const see_all_theme = document.querySelectorAll(".see-all-theme");
 
-  //非表示イベント
-  if (all_theme[i].classList.contains("show-all-theme")) {
-    all_theme[i].classList.remove("show-all-theme");
-    button_arrow.classList.remove("rotate-arrow");
-    see_all_theme[i].style.borderBottom = "solid 2px #fff";//borderの重なりを防止
+//   //非表示イベント
+//   if (all_theme[i].classList.contains("show-all-theme")) {
+//     all_theme[i].classList.remove("show-all-theme");
+//     button_arrow.classList.remove("rotate-arrow");
+//     see_all_theme[i].style.borderBottom = "solid 2px #fff";//borderの重なりを防止
 
-    //all-themeの非表示
-    all_theme[i].style.maxHeight = "0";
-    setTimeout( () => {
-      all_theme[i].style.border = "solid 0px #fff";
-      all_theme[i].style.padding = "0";
-    },650);
-  //表示イベント
-  } else {
-    all_theme[i].classList.add("show-all-theme");
-    button_arrow.classList.add("rotate-arrow");
-    see_all_theme[i].style.borderBottom = "none";//borderの重なりを防止
+//     //all-themeの非表示
+//     all_theme[i].style.maxHeight = "0";
+//     setTimeout( () => {
+//       all_theme[i].style.border = "solid 0px #fff";
+//       all_theme[i].style.padding = "0";
+//     },650);
+//   //表示イベント
+//   } else {
+//     all_theme[i].classList.add("show-all-theme");
+//     button_arrow.classList.add("rotate-arrow");
+//     see_all_theme[i].style.borderBottom = "none";//borderの重なりを防止
 
-    //all-themeの表示
-    all_theme[i].style.border = "solid 30px #fff";
-    all_theme[i].style.padding = "20px";
-    all_theme[i].style.maxHeight = "1300px";
-  }
-}
+//     //all-themeの表示
+//     all_theme[i].style.border = "solid 30px #fff";
+//     all_theme[i].style.padding = "20px";
+//     all_theme[i].style.maxHeight = "1300px";
+//   }
+// }
 
 //===スムーズスクロール
 $(function () {
@@ -73,37 +73,96 @@ function goMap() {
   window.open(url, "_blank");
 }
 
-//===強制水平スクロール
-const horizonflow = document.querySelectorAll("[data-horizonflow]");
-let isScrolling = false;
+// //===強制水平スクロール
+// const horizonflow = document.querySelectorAll("[data-horizonflow]");
+// let isScrolling = false;
 
-horizonflow.forEach(flow => {
-  flow.style.setProperty("height", `${Number(flow.dataset.horizonflow) * 100}vh`);
+// horizonflow.forEach(flow => {
+//   flow.style.setProperty("height", `${Number(flow.dataset.horizonflow) * 100}vh`);
+// });
+
+// window.addEventListener("scroll", () => {
+//   if (!isScrolling) {
+//     window.requestAnimationFrame(() => {
+//       horizonflow.forEach(flow => {
+//         const content = [...flow.children].pop();
+//         const rect = {
+//           flow: flow.getBoundingClientRect(),
+//           content: content.getBoundingClientRect()
+//         };
+//         const rate = Math.min(
+//           Math.max(-rect.flow.top / (rect.flow.height - rect.content.height), 0),
+//           1
+//         );
+//         content.scrollTo(rate * (content.scrollWidth - rect.content.width), 0);
+//       });
+//       isScrolling = false;
+//     });
+//     isScrolling = true;
+//   }
+// });
+
+//===programでスクロールを促す矢印の非表示
+document.addEventListener("DOMContentLoaded", function () {
+  const arrows = document.getElementById("program-arrows");
+  document.getElementById("program-list").addEventListener("scroll", () => {
+    if(arrows.style.display == "none"){
+    } else {
+      arrows.style.display = "none";
+    }
+  });
 });
 
-window.addEventListener("scroll", () => {
-  if (!isScrolling) {
-    window.requestAnimationFrame(() => {
-      horizonflow.forEach(flow => {
-        const content = [...flow.children].pop();
-        const rect = {
-          flow: flow.getBoundingClientRect(),
-          content: content.getBoundingClientRect()
-        };
-        const rate = Math.min(
-          Math.max(-rect.flow.top / (rect.flow.height - rect.content.height), 0),
-          1
-        );
-        content.scrollTo(rate * (content.scrollWidth - rect.content.width), 0);
-      });
-      isScrolling = false;
-    });
-    isScrolling = true;
-  }
+//===plane-imgの表示/非表示
+// const planeimg = document.getElementById("plane-img");
+// let borderline;
+// let elementRight;
+// document.addEventListener("DOMContentLoaded", function () {
+//     window.addEventListener("scroll", () => {
+//       if(planeimg.getBoundingClientRect().left < 0){
+//         elementRight = (window.innerWidth - planeimg.getBoundingClientRect().left) - planeimg.width;
+//       }else if(planeimg.getBoundingClientRect().left >= 0){
+//         elementRight = window.innerWidth - (planeimg.getBoundingClientRect().left + planeimg.width);
+//       }
+//       console.log(elementRight);
+//       //ボーダー下
+//       if(elementRight <= 0){
+//         planeimg.style.display = "none";
+//         borderline = window.scrollY - 50;
+//       //ボーダー上
+//       }else if(borderline > window.scrollY){
+//         planeimg.style.display = "block";
+//       }
+//     });
+// });
+
+//===plane-imgによる画面幅バグの修正
+const planeimg = document.getElementById("plane-img");
+let borderline = 0;
+let elementRight;
+
+document.addEventListener("DOMContentLoaded", function () {
+  window.addEventListener("scroll", () => {
+    if (planeimg.getBoundingClientRect().left < 0) {
+      elementRight = (window.innerWidth - planeimg.getBoundingClientRect().left) - planeimg.width;
+    } else if (planeimg.getBoundingClientRect().left >= 0) {
+      elementRight = window.innerWidth - (planeimg.getBoundingClientRect().left + planeimg.width);
+    }
+    // console.log(elementRight);
+
+    // ボーダー下
+    if (elementRight <= 0) {
+      planeimg.style.display = "none";
+      borderline = window.scrollY + 50; // borderlineの値を更新（+50）
+    } else if (borderline > window.scrollY) {
+      // ボーダー上
+      planeimg.style.display = "block";
+    }
+  });
 });
 
 
-//first-view背景のレスポンシブ(画像切り替え)
+//===first-view背景のレスポンシブ(画像切り替え)
 document.addEventListener("DOMContentLoaded", function () {
   imgExchange();
   window.addEventListener("resize", function () {
